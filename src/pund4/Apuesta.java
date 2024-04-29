a11b459fe16800d732eeda59b90fc97c33a76e7b
Paquito <alvarogjuarez@gmail.com>
2024-04-29 09:23:24 +0200

    Ejercicios master hechos

--- src/pund4/Apuesta.java
+++ src/pund4/Apuesta.java
@@ -7,45 +7,72 @@ package pund4;
 
 public class Apuesta {
 
-   
+   /**
+    * @author Alvaro
+    * @return devuelve la cantidad de goles locales 
+    */
     public int getGoles_local() {
         return goles_local;
     }
 
-    
+    /**
+     * @author Alvaro
+     * @param goles_local cantidad de goles locales
+     */
     public void setGoles_local(int goles_local) {
         this.goles_local = goles_local;
     }
 
-   
+   /**
+    * @author Alvaro
+    * @return cantidad de goles visitantes
+    */
     public int getGoles_visitante() {
         return goles_visitante;
     }
 
-    
+    /**
+     * @author Alvaro
+     * @param goles_visitante cantidad de goles visitantes
+     */
     public void setGoles_visitante(int goles_visitante) {
         this.goles_visitante = goles_visitante;
     }
 
-    
+    /**
+     * @author Alvaro
+     * @return cantidad de dinero apostado
+     */
     public int getApostado() {
         return apostado;
     }
 
-    
+    /**
+     * @author Alvaro
+     * @param apostado cantidad de dinero apostado
+     */
     public void setApostado(int apostado) {
         this.apostado = apostado;
     }
 
-    private int dinero_disp;
-    private int goles_local;
-    private int goles_visitante;
-    private int apostado;
-
+    public int dinero_disp;
+    public int goles_local;
+    public int goles_visitante;
+    public int apostado;
+    
+    /**
+     * @author Alvaro
+     */
     /*Contructor por defecto*/
     public Apuesta() {
     }
-
+    
+    /**
+     * @author Alvaro
+     * @param dinero_disp cantidad de dinero disponible
+     * @param goles_local cantidad de goles locales
+     * @param goles_visitante cantidad de goles visitantes
+     */
     /*Contructor con parámetros*/
     public Apuesta(int dinero_disp, int goles_local, int goles_visitante) {
         this.dinero_disp = dinero_disp;
@@ -53,17 +80,33 @@ public class Apuesta {
         this.goles_visitante = goles_visitante;
         this.apostado = 0;
     }
+    
+    /**
+     * @author Alvaro
+     * @return cantidad de dinero disponible
+     */
     /*Método para obtener el valor del atributo dinero_disp*/
 
     public int getDinero_disp() {
         return dinero_disp;
     }
+    
+    /**
+     * @author Alvaro
+     * @param dinero_disp cantidad de dinero disponible
+     */
     /*Método para modificar el valor del atributo dinero_disp*/
 
     public void setDinero_disp(int dinero_disp) {
         this.dinero_disp = dinero_disp;
     }
-
+    
+    
+    /**
+     * @author Alvaro
+     * @param dinero cantidad de dinero que se apuesta
+     * @throws Exception Excepción si se apuesta menos de uno o más del dinero que se tiene
+     */
     /*Método para apostar.
      * Permite elegir la cantidad a apostar, no pudiendo ser inferior a 1 ni superior a tu saldo disponible
      * Este método será probado con JUnit
@@ -73,11 +116,11 @@ public class Apuesta {
             throw new Exception("No se puede apostar menos de 1€");
         }
 
-        if (dinero > dinero_disp) {
+        if (dinero > getDinero_disp()) {
             throw new Exception("No se puede apostar mas de lo que tienes");
         }
         {
-            dinero_disp = dinero - dinero_disp;
+            setDinero_disp(dinero - getDinero_disp());
             setApostado(dinero);
         }
     }
@@ -85,7 +128,13 @@ public class Apuesta {
      * En caso de que lo haya acertado devuelve true. Chequea que no se metan menos de 0 goles
      * 
      */
-
+    /**
+     * @author Alvaro
+     * @param local cantidad de goles locales
+     * @param visitante cantidad de goles visitantes
+     * @return se ha acertado o no
+     * @throws Exception si el valor de los parametros es 0
+     */
     public boolean comprobar_resultado(int local, int visitante) throws Exception {
         boolean acertado = false;
         if ((local < 0) || (visitante) < 0) {
@@ -102,13 +151,18 @@ public class Apuesta {
      * al saldo disponible
      * Este método se va a probar con Junit
      */
-
+    /**
+     * @author Alvaro
+     * @param cantidad_goles_local cantidad de goles locales
+     * @param cantidad_goles_visit cantidad de goles visitantes
+     * @throws Exception Si la apuesta es incorrecta, no se no se cobra
+     */
     void cobrar_apuesta(int cantidad_goles_local, int cantidad_goles_visit) throws Exception {
 
         if (comprobar_resultado(cantidad_goles_local, cantidad_goles_visit) == false) {
             throw new Exception("No se puede cobrar una apuesta no acertada");
         }
-        dinero_disp = dinero_disp * 10;
+        setDinero_disp(getDinero_disp() * 10);
 
     }
 }
--- src/pund4/PUND4.java
+++ src/pund4/PUND4.java
@@ -14,24 +14,27 @@ public class PUND4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Apuesta miApuesta;
-        int mi_dinero;
+       Apuesta laApuesta;
 
-        miApuesta = new Apuesta(1000, 4, 2);
+        laApuesta = new Apuesta(1000, 4, 2);
+        operativaApuesta(laApuesta, 0);
+    }
+
+    public static void operativaApuesta(Apuesta laApuesta, int dinero) {
+        int mi_dinero;
         try {
             System.out.println("Apostando...");
-            miApuesta.apostar(25);
+            laApuesta.apostar(25);
         } catch (Exception e) {
             System.out.println("Fallo al realizar la Apuesta");
         }
-
         try {
             System.out.println("Intento cobrar apuesta segun el resultado del partido");
-            miApuesta.cobrar_apuesta(2, 3);
+            laApuesta.cobrar_apuesta(2, 3);
         } catch (Exception e) {
             System.out.println("Fallo al cobrar la apuesta");
         }
-        mi_dinero = miApuesta.getDinero_disp();
+        mi_dinero = laApuesta.getDinero_disp();
         System.out.println("El dinero que tengo tras las apuestas es " + mi_dinero);
     }
     
